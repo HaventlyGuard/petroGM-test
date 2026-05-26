a2db511daf3d7f92e8c31f48ce089845472a45a2
Lyamin Timofey <162110108+HaventlyGuard@users.noreply.github.com>
2026-05-26 19:33:17 +0700

    add Circle.cs

---
+++ GeometryApp/GeometryLib/Features/Circle/Circle.cs
@@ -0,0 +1,27 @@
+﻿using GeometryLib.Core;
+
+namespace GeometryLib.Features.Circle
+{
+    public class Circle : IShape
+    {
+        public PointD Center { get; }
+        public double Radius { get; }
+        public string ShapeType => "circle";
+        
+        public Circle(double x, double y, double radius) 
+            : this(new PointD(x, y), radius)
+        {
+        }
+        
+        public Circle(PointD center, double radius)
+        {
+            Center = center;
+            Radius = radius;
+        }
+        
+        public string Draw() => $"circle at {Center}, radius = {Radius}";
+        
+        public double X => Center.X;
+        public double Y => Center.Y;
+    }
+}

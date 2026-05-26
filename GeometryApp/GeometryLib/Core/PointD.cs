097370586c087c6e16055bf6acea4b0cc8713320
Lyamin Timofey <162110108+HaventlyGuard@users.noreply.github.com>
2026-05-26 21:09:54 +0700

    Add ShapeFactory.cs & fix bugs

--- GeometryApp/GeometryLib/Core/PointD.cs
+++ GeometryApp/GeometryLib/Core/PointD.cs
@@ -3,32 +3,33 @@
 namespace GeometryLib.Core
 {
     public readonly struct PointD
+
     {
-        public double X { get; }
-        public double Y { get; }
-        
-        public PointD(double x, double y)
-        {
-            X = x;
-            Y = y;
-        }
-        
-        public override string ToString() => $"({X}, {Y})";
-        
-        public bool Equals(PointD other) 
-            => Math.Abs(X - other.X) < 1e-9 && Math.Abs(Y - other.Y) < 1e-9;
-            
-        public override bool Equals(object? obj) 
-            => obj is PointD other && Equals(other);
-            
-        public override int GetHashCode() 
-            => HashCode.Combine(X, Y);
-            
-        public static bool operator ==(PointD left, PointD right) 
-            => left.Equals(right);
-            
-        public static bool operator !=(PointD left, PointD right) 
-            => !left.Equals(right);
-      
+    public double X { get; }
+    public double Y { get; }
+
+    public PointD(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public override string ToString() => $"({X}, {Y})";
+
+    public bool Equals(PointD other)
+        => Math.Abs(X - other.X) < 1e-9 && Math.Abs(Y - other.Y) < 1e-9;
+
+    public override bool Equals(object? obj)
+        => obj is PointD other && Equals(other);
+
+    public override int GetHashCode()
+        => HashCode.Combine(X, Y);
+
+    public static bool operator ==(PointD left, PointD right)
+        => left.Equals(right);
+
+    public static bool operator !=(PointD left, PointD right)
+        => !left.Equals(right);
+
     }
 }

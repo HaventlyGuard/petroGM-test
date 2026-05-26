88feab87ea5c321ce53ea6b8b4e491d4acd8da4f
Lyamin Timofey <162110108+HaventlyGuard@users.noreply.github.com>
2026-05-26 18:55:58 +0700

    add Point.cs

---
+++ GeometryApp/GeometryLib/Features/Point/Point.cs
@@ -0,0 +1,23 @@
+﻿using GeometryLib.Core;
+
+namespace GeometryLib.Features.Point;
+
+public class Point : IShape
+{
+    private PointD Position { get; set; }
+
+    public Point(PointD pos)
+    {
+        Position = pos;
+    }
+    
+    public string Draw()
+    {
+        return $"point at position: X: {Position.X}, Y: {Position.Y}";
+    }
+
+    public IntersectionResult Intersect(IShape other)
+    {
+        return IntersectionResult.CannotIntersect(this, other);
+    }
+}

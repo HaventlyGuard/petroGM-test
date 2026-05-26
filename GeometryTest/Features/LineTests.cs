a1da89162f161e6eca3cf8c9488560c5cb6a1910
Lyamin Timofey <162110108+HaventlyGuard@users.noreply.github.com>
2026-05-26 21:50:44 +0700

    Point & Line tests

---
+++ GeometryTest/Features/LineTests.cs
@@ -0,0 +1,44 @@
+﻿namespace GeometryTest.Features
+{
+    public class LineTests
+    {
+        [Fact]
+        public void Draw_ReturnsCorrectString()
+        {
+            var line = new Line(new PointD(0, 0), new PointD(10, 10));
+            
+            var result = line.Draw();
+            
+            Assert.Equal("line from (0.00, 0.00) to (10.00, 10.00)", result);
+        }
+        
+        [Fact]
+        public void ShapeType_ReturnsLine()
+        {
+            var line = new Line(new PointD(0, 0), new PointD(1, 1));
+            
+            Assert.Equal("line", line.ShapeType);
+        }
+        
+        [Fact]
+        public void Length_CalculatesCorrectly()
+        {
+            var line = new Line(new PointD(0, 0), new PointD(3, 4));
+            
+            var length = line.Length;
+            
+            Assert.Equal(5, length, 1e-9);
+        }
+        
+        [Fact]
+        public void Constructor_WithDoubles_CreatesCorrectLine()
+        {
+            var line = new Line(new PointD(1,2), new PointD(3, 4));
+            
+            Assert.Equal(1, line.pointA.X);
+            Assert.Equal(2, line.pointA.Y);
+            Assert.Equal(3, line.pointB.X);
+            Assert.Equal(4, line.pointB.Y);
+        }
+    }
+}
---
+++ GeometryTest/Features/PointTests.cs
@@ -0,0 +1,33 @@
+﻿namespace GeometryTest.Features
+{
+    public class PointTests
+    {
+        [Fact]
+        public void Draw_ReturnsCorrectString()
+        {
+            var point = new Point(new PointD(10, 20));
+            
+            var result = point.Draw();
+            
+            Assert.Equal("point at (10.00, 20.00)", result);
+        }
+        
+        [Fact]
+        public void ShapeType_ReturnsPoint()
+        {
+            var point = new Point(new PointD(1,2));
+            
+            Assert.Equal("point", point.ShapeType);
+        }
+        
+        [Fact]
+        public void Constructor_FromPointD()
+        {
+            var position = new PointD(5, 5);
+            var point = new Point(position);
+            
+            Assert.Equal(5, point.X);
+            Assert.Equal(5, point.Y);
+        }
+    }
+}

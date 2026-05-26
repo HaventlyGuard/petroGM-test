eae69a877d2f0ea5ed3fea76c1575c93db044710
Lyamin Timofey <162110108+HaventlyGuard@users.noreply.github.com>
2026-05-26 21:43:46 +0700

    add PointD tests

---
+++ GeometryTest/Core/PointDTests.cs
@@ -0,0 +1,42 @@
+﻿namespace GeometryTest.Core;
+
+public class PointDTests
+{
+    [Fact]
+    public void ConstructorTest()
+    {
+        var point = new PointD(10, 7.2);
+        Assert.Equal(10, point.X);
+        Assert.Equal(7.2, point.Y);
+    }
+    
+    [Fact]
+    public void ToString_ReturnsFormattedPoint()
+    {
+        var point = new PointD(1.5, 2.5);
+            
+        var result = point.ToString();
+            
+        Assert.Equal("(1.50, 2.50)", result);
+    }
+        
+    [Fact]
+    public void Equals_True_ForSameCoordinates()
+    {
+        var point1 = new PointD(1, 2);
+        var point2 = new PointD(1, 2);
+            
+        Assert.Equal(point1, point2);
+        Assert.True(point1 == point2);
+    }
+        
+    [Fact]
+    public void Equals_False_ForDifferentCoordinates()
+    {
+        var point1 = new PointD(1, 2);
+        var point2 = new PointD(3, 4);
+            
+        Assert.NotEqual(point1, point2);
+        Assert.True(point1 != point2);
+    }
+}
---
+++ GeometryTest/Core/Usings.cs
@@ -0,0 +1,8 @@
+﻿global using Xunit;
+global using GeometryLib.Core;
+global using GeometryLib.Features.Point;
+global using GeometryLib.Features.Line;
+global using GeometryLib.Features.Rect;
+global using GeometryLib.Features.Circle;
+global using GeometryLib.Services;
+global using GeometryLib.Factory;

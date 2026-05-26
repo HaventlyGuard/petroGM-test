64858761db81abafda9fb51e9e2d7ba51e2c4930
Lyamin Timofey <162110108+HaventlyGuard@users.noreply.github.com>
2026-05-26 18:44:18 +0700

    add IntersectionResult.cs

---
+++ GeometryApp/GeometryLib/Core/IntersectionResult.cs
@@ -0,0 +1,25 @@
+﻿namespace GeometryLib.Core
+{
+    public class IntersectionResult
+    {
+        public bool HasIntersection { get; }
+        public IReadOnlyList<PointD> Points { get; }
+        public string Message { get; }
+        
+        private IntersectionResult(bool hasIntersection, IReadOnlyList<PointD> points, string message)
+        {
+            HasIntersection = hasIntersection;
+            Points = points;
+            Message = message;
+        }
+        
+        public static IntersectionResult Intersects(params PointD[] points) 
+            => new(true, points, $"Intersection at {string.Join(" and ", points)}");
+            
+        public static IntersectionResult NoIntersection() 
+            => new(false, Array.Empty<PointD>(), "No intersections");
+            
+        public static IntersectionResult CannotIntersect(IShape shape1, IShape shape2) 
+            => new(false, Array.Empty<PointD>(), $"The {shape1.GetType().Name.ToLower()} cannot intersect the {shape2.GetType().Name.ToLower()}");
+    }
+}

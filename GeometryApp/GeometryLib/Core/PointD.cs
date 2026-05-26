42eed9018619d3666973fd0218c139bb41a1d1c9
Lyamin Timofey <162110108+HaventlyGuard@users.noreply.github.com>
2026-05-26 18:40:28 +0700

    Add struct PointD.cs for points

---
+++ GeometryApp/GeometryLib/Core/PointD.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace GeometryLib.Core
+{
+    public readonly struct PointD
+    {
+        public double X { get; }
+        public double Y { get; }
+        
+        public PointD(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        
+        public override string ToString() => $"({X}, {Y})";
+        public bool Equals(PointD other) => X.Equals(other.X) && Y.Equals(other.Y);
+      
+    }
+}

394342580f3fd03312b04a7b3f6a2cc2c4e00eb5
Lyamin Timofey <162110108+HaventlyGuard@users.noreply.github.com>
2026-05-26 21:55:14 +0700

    Circle & Rect tests

---
+++ GeometryTest/Features/CircleTests.cs
@@ -0,0 +1,33 @@
+﻿namespace GeometryTest.Features
+{
+    public class CircleTests
+    {
+        [Fact]
+        public void Draw_ReturnsCorrectString()
+        {
+            var circle = new Circle(new PointD(-100, 300), 50);
+            
+            var result = circle.Draw();
+            
+            Assert.Equal("circle at (-100.00, 300.00), radius = 50", result);
+        }
+        
+        [Fact]
+        public void ShapeType_ReturnsCircle()
+        {
+            var circle = new Circle(new PointD(0, 0), 10);
+            
+            Assert.Equal("circle", circle.ShapeType);
+        }
+        
+        [Fact]
+        public void Constructor_WithDoubles_CreatesCorrectCircle()
+        {
+            var circle = new Circle(5, 5, 10);
+            
+            Assert.Equal(5, circle.Center.X);
+            Assert.Equal(5, circle.Center.Y);
+            Assert.Equal(10, circle.Radius);
+        }
+    }
+}
---
+++ GeometryTest/Features/RectTests.cs
@@ -0,0 +1,56 @@
+﻿namespace GeometryTest.Features
+{
+    public class RectTests
+    {
+        [Fact]
+        public void Draw_ReturnsCorrectString()
+        {
+            var rect = new Rect(new PointD(1, 2), new PointD(3, 4));
+            
+            var result = rect.Draw();
+            
+            Assert.Equal("rect at (1.00, 2.00), (3.00, 4.00)", result);
+        }
+        
+        [Fact]
+        public void NormalizesCoordinates()
+        {
+            var rect = new Rect(new PointD(10, 10), new PointD(0, 0));
+            
+            Assert.Equal(0, rect.Left);
+            Assert.Equal(0, rect.Top);
+            Assert.Equal(10, rect.Right);
+            Assert.Equal(10, rect.Bottom);
+        }
+        
+        [Fact]
+        public void GetEdges_ReturnsFourEdges()
+        {
+            var rect = new Rect(new PointD(0, 0), new PointD(2, 2));
+            
+            var edges = rect.GetEdges();
+            
+            Assert.Equal(4, edges.Length);
+        }
+        
+        [Fact]
+        public void Width_ReturnsCorrectValue()
+        {
+            var rect = new Rect(new PointD(0, 0), new PointD(5, 10));
+            
+            Assert.Equal(5, rect.Width);
+            Assert.Equal(10, rect.Height);
+        }
+        
+        [Fact]
+        public void Constructor_WithDoubles_CreatesCorrectRect()
+        {
+            var rect = new Rect(1, 2, 3, 4);
+            
+            Assert.Equal(1, rect.Left);
+            Assert.Equal(2, rect.Top);
+            Assert.Equal(3, rect.Right);
+            Assert.Equal(4, rect.Bottom);
+        }
+    }
+}
